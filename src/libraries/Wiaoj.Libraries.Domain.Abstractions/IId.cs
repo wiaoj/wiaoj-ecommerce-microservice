df2c24575e7d8c0b289f5deacabc946dfcdd526f
wiaoj <0.wiaoj@gmail.com>
2024-07-05 11:31:04 +0300

    refactor: Improved code structure in aggregate and ID files

--- src/libraries/Wiaoj.Libraries.Domain.Abstractions/IId.cs
+++ src/libraries/Wiaoj.Libraries.Domain.Abstractions/IId.cs
@@ -1,6 +1,8 @@
 ﻿namespace Wiaoj.Libraries.Domain.Abstractions;
-public interface IId<out T> where T : IValueObject {
-    public T Id { get; }
+public interface IId<out T> {
+    public T Value { get; }
 }
+
+public interface IId : IId<String>;
 
 public interface IValueObject { }
