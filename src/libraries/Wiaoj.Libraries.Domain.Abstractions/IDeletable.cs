c02cf2c8f61ab7d0d51d4b56aed274b45db15a28
wiaoj <0.wiaoj@gmail.com>
2024-07-05 11:09:07 +0300

    (feat): Added Domain Abstract Library and Publishing Local Nuget Server via Baget

---
+++ src/libraries/Wiaoj.Libraries.Domain.Abstractions/IDeletable.cs
@@ -0,0 +1,7 @@
+﻿namespace Wiaoj.Libraries.Domain.Abstractions;
+public interface IDeletable {
+    Boolean IsDeleted { get; }
+    DateTime? DeletedAt { get; }
+    void Delete(DateTime deletedAt);
+    void Restore();
+}
---
+++ src/libraries/Wiaoj.Libraries.Domain.Abstractions/IId.cs
@@ -0,0 +1,6 @@
+﻿namespace Wiaoj.Libraries.Domain.Abstractions;
+public interface IId<out T> where T : IValueObject {
+    public T Id { get; }
+}
+
+public interface IValueObject { }
