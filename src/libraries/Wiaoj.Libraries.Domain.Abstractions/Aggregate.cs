d78ddd4bb2a4e49927a45f6b86fe34f13d3b76b9
wiaoj <0.wiaoj@gmail.com>
2024-08-06 23:33:20 +0300

    (feature): Added domain event

--- src/libraries/Wiaoj.Libraries.Domain.Abstractions/Aggregate.cs
+++ src/libraries/Wiaoj.Libraries.Domain.Abstractions/Aggregate.cs
@@ -1,13 +1,17 @@
-﻿using Wiaoj.Libraries.Domain.Abstractions.Exceptions;
+﻿using Wiaoj.Libraries.Domain.Abstractions.DomainEvents;
+using Wiaoj.Libraries.Domain.Abstractions.Exceptions;
 
 namespace Wiaoj.Libraries.Domain.Abstractions;
-public abstract class Aggregate<TId> : IAggregate
+public abstract class Aggregate<TId> : IAggregate, IHasDomainEvent
     where TId : class, IId<TId> {
     public TId Id { get; private set; }
     public DateTime CreatedAt { get; private set; }
     public DateTime? UpdatedAt { get; private set; }
     public Boolean IsDeleted { get; private set; }
     public DateTime? DeletedAt { get; private set; }
+
+    private readonly List<IDomainEvent> domainEvents = [];
+    public IReadOnlyList<IDomainEvent> DomainEvents => this.domainEvents.AsReadOnly();
 
 #pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
     protected Aggregate() { }
@@ -38,5 +42,14 @@ public abstract class Aggregate<TId> : IAggregate
 
     public void SetUpdatedAt(DateTime updatedAt) {
         this.UpdatedAt = updatedAt;
+    }
+
+    public void AddDomainEvent(IDomainEvent @event) {
+        if(!this.domainEvents.Exists(x => x == @event)) 
+            this.domainEvents.Add(@event);
+    }
+
+    public void ClearDomainEvents() {
+        this.domainEvents.Clear();
     }
 }
