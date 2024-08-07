d9c9189dc0a635e76863bd5e1de443d721f88a7f
wiaoj <0.wiaoj@gmail.com>
2024-08-07 17:02:12 +0300

    (feature): Added Serilog

--- src/services/CatalogDefinitionService/Wiaoj.ECommerce.CatalogDefinitionService.Infrastructure/DependencyInjection.cs
+++ src/services/CatalogDefinitionService/Wiaoj.ECommerce.CatalogDefinitionService.Infrastructure/DependencyInjection.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+using Serilog.Settings.Configuration;
 using Wiaoj.ECommerce.CatalogDefinitionService.Application.Abstractions;
 using Wiaoj.ECommerce.CatalogDefinitionService.Domain.CatalogItemAggregate.Services;
 using Wiaoj.ECommerce.CatalogDefinitionService.Infrastructure.Events;
@@ -6,7 +9,13 @@ using Wiaoj.ECommerce.CatalogDefinitionService.Infrastructure.Services;
 
 namespace Wiaoj.ECommerce.CatalogDefinitionService.Infrastructure;
 public static class DependencyInjection {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services) {
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services,
+                                                               IConfiguration configuration) {
+        services.AddSerilog(configure => {
+            configure.ReadFrom.Configuration(configuration, new ConfigurationReaderOptions() {
+                SectionName = "Serilog"
+            });
+        });
         services.AddSingleton<IDateTimeProvider, SystemDateTimeProvider>();
         services.AddSingleton<IInMemoryMessageQueue, InMemoryMessageQueue>();
         services.AddSingleton<IDomainEventBus, InMemoryDomainEventBus>();
