fce3f096a4f22ae22ac5cbc5de7e898f475106a4
wiaoj <0.wiaoj@gmail.com>
2024-08-08 02:39:19 +0300

    (feature): Catalog Discovery Service added to Catalog Gateway

--- src/services/CatalogDiscoveryService/Wiaoj.ECommerce.CatalogDiscoveryService.WebAPI/Program.cs
+++ src/services/CatalogDiscoveryService/Wiaoj.ECommerce.CatalogDiscoveryService.WebAPI/Program.cs
@@ -1,32 +1,18 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
-// Add services to the container.
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+builder.WebHost.ConfigureKestrel((options) => {
+    options.ConfigureEndpointDefaults(configure => {
+        configure.Protocols = HttpProtocols.Http3;
+        configure.UseHttps();
+    });
+});
+
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () => {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-});
-
-app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary) {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+await app.RunAsync(default(CancellationToken));
