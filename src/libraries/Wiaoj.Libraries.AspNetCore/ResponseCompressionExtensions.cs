2defe4bf91857dc4796f138bcb37ef65396ec864
wiaoj <0.wiaoj@gmail.com>
2024-08-07 16:53:19 +0300

    (refactor): Added Json Options & ResponseCompression

---
+++ src/libraries/Wiaoj.Libraries.AspNetCore/ResponseCompressionExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO.Compression;
+
+namespace Wiaoj.Libraries.AspNetCore;
+public static class ResponseCompressionExtensions {
+    public static IServiceCollection AddCustomResponseCompression(this IServiceCollection services) {
+        return services.AddResponseCompression(options => {
+            options.EnableForHttps = true;
+            options.Providers.Add<BrotliCompressionProvider>();
+            options.Providers.Add<GzipCompressionProvider>();
+        }).Configure<BrotliCompressionProviderOptions>(options => {
+            options.Level = CompressionLevel.Fastest;
+        }).Configure<GzipCompressionProviderOptions>(options => {
+            options.Level = CompressionLevel.SmallestSize;
+        });
+    }
+}
