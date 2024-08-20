0a5e730f4f5ea70e689ffef1ef5b3071c3d93f50
wiaoj <0.wiaoj@gmail.com>
2024-08-20 22:48:44 +0300

    (-) test for elastic client

--- src/services/CatalogDiscoveryService/Wiaoj.ECommerce.CatalogDiscoveryService.WebAPI/Program.cs
+++ src/services/CatalogDiscoveryService/Wiaoj.ECommerce.CatalogDiscoveryService.WebAPI/Program.cs
@@ -1,18 +1,36 @@
+using Elastic.Clients.Elasticsearch.QueryDsl;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Wiaoj.ECommerce.CatalogDiscoveryService.WebAPI.Repositories;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 builder.WebHost.ConfigureKestrel((options) => {
     options.ConfigureEndpointDefaults(configure => {
-        configure.Protocols = HttpProtocols.Http3;
+        configure.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
         configure.UseHttps();
     });
 });
+
+builder.Services.AddCors(setup => {
+    setup.AddDefaultPolicy(policy => {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddScoped<ElasticsearchCatalogItemRepository>();
 
 WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+app.UseCors();
 app.UseHttpsRedirection();
+
+app.MapGet("api/{page}/{size}", ([FromRoute] Int32 page, [FromRoute] Int32 size, ElasticsearchCatalogItemRepository a) => {
+    return Results.Ok(a.SearchAsync(page, size));
+});
 
 await app.RunAsync(default(CancellationToken));
