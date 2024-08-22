3985d24a9b69e98d013a13604bcaeaf1821455c3
wiaoj <0.wiaoj@gmail.com>
2024-08-22 14:02:53 +0300

    (feature) added repository package

---
+++ src/libraries/Wiaoj.Libraries.Repository.Abstractions/IGetByIdRepository.cs
@@ -0,0 +1,7 @@
+﻿using Wiaoj.Libraries.Domain.Abstractions;
+using Wiaoj.Libraries.Domain.Abstractions.ValueObjects;
+
+namespace Wiaoj.Libraries.Repository.Abstractions;
+public interface IGetByIdRepository<TEntity, in TEntityId> where TEntity : Aggregate<TEntityId> where TEntityId : class, IId<TEntityId> {
+    Task<TEntity?> GetByIdAsync(TEntityId id, CancellationToken cancellationToken);
+}
---
+++ src/libraries/Wiaoj.Libraries.Repository.Abstractions/IInsertRepository.cs
@@ -0,0 +1,6 @@
+﻿using Wiaoj.Libraries.Domain.Abstractions;
+
+namespace Wiaoj.Libraries.Repository.Abstractions;
+public interface IInsertRepository<in TEntity> where TEntity : IAggregate {
+    Task InsertAsync(TEntity entity, CancellationToken cancellationToken);
+}
