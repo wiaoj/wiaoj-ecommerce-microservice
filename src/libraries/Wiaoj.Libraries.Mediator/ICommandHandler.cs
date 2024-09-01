51ce85cdca5136543bc8898f26e3908a970a68af
wiaoj <0.wiaoj@gmail.com>
2024-09-01 21:54:20 +0300

    (feature) added mediator query

---
+++ src/libraries/Wiaoj.Libraries.Mediator/ICommandHandler.cs
@@ -0,0 +1,5 @@
+﻿namespace Wiaoj.Libraries.Mediator.Abstractions;
+public interface ICommandHandler<TCommand> : BaseMediator.ICommandHandler<TCommand> where TCommand : ICommand;
+public interface ICommandHandler<in TCommand, TResponse> : BaseMediator.ICommandHandler<TCommand, TResponse>
+    where TCommand : ICommand<TResponse>
+    where TResponse : notnull;
